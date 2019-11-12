805c1121680c3e4052e0a774a02c5f2fff7b3a97
Glitch (dayscraper) <none>
2019-11-12 22:43:39 +0000

    Update

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import {FaGithub,FaLink,FaEnvelope} from "react-icons/fa"
 const Footer = ()=> {
     return(
         <div className="footer">
-            <a href="https://github.com/hacker719/dayscraper" className="item" style={{margin:"auto",textDecoration:"none"}}>@Hacker719/DayScraper</a>
+            <a href="https://github.com/neelr/dayscraper" className="item" style={{margin:"auto",textDecoration:"none"}}>@neelr/DayScraper</a>
             <div style={{margin:"auto",display:"flex", color:"#2970f2"}}>
-                <a href="https://github.com/hacker719"><FaGithub className="item" size="1.5em" style={{margin:"10px"}}/></a>
+                <a href="https://github.com/neelr"><FaGithub className="item" size="1.5em" style={{margin:"10px"}}/></a>
                 <a href="mailto:neel.redkar@outlook.com"><FaEnvelope className="item" style={{margin:"10px"}} size="1.5em"/></a>
                 <a href="https://neelr.dev/"><FaLink className="item" style={{margin:"10px"}} size="1.5em"/></a>
             </div>
